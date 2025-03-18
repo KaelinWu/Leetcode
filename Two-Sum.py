6dc174b2cde59b40e027c312ede1cb928d00e204
Kaelin Wu <152450138+KaelinWu@users.noreply.github.com>
2025-03-18 00:40:10 -0400

    [1] [Time Beats: 51.15%] [Memory Beats: 51.25%] - LeetPush

---
+++ Two-Sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        my_dict = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in my_dict:
+                return [i, my_dict[diff]]
+            my_dict[nums[i]] = i
+        return False
+           
+
+        
