2812ed0a872ddf6e31c731c2c87d5a25274c3cc5
Kaelin Wu <152450138+KaelinWu@users.noreply.github.com>
2025-03-18 00:17:49 -0400

    [242] [Time Beats: 47.06%] [Memory Beats: 33.74%] - LeetPush

---
+++ Valid-Anagram.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_set = {}
+        t_set = {}
+        if len(s) != len(t):
+            return False
+        for i in range(len(s)):
+            s_set[s[i]] = s_set.get(s[i], 0) + 1
+            t_set[t[i]] = t_set.get(t[i], 0) + 1
+
+        if s_set == t_set:
+            return True
+        return False
+
+        
