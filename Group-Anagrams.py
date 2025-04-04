5bee06f67775a920112f0e24a84685c1d22f96da
Kaelin Wu <152450138+KaelinWu@users.noreply.github.com>
2025-04-03 23:10:38 -0400

    [49] [Time Beats: 98.02%] [Memory Beats: 63.62%] - LeetPush

--- Group-Anagrams.py
+++ Group-Anagrams.py
@@ -1,10 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mp = defaultdict(list)
-        for word in strs:
-            sorted_word = "".join(sorted(word))
-            mp[sorted_word].append(word)
-        return list(mp.values())
-            
+        map = defaultdict(list)
+        
+        for name in strs:
+            word = ''.join(sorted(name))
+            map[word].append(name)
 
+        return list(map.values())
+            
+        
+        
+    
         
