5787063f0bfc47ad1afa8119c3ea0b525aa9cadd
Kaelin Wu <152450138+KaelinWu@users.noreply.github.com>
2025-04-30 16:33:39 -0400

    [121] [Time Beats: 82.41%] [Memory Beats: 78.01%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock.py
+++ Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,17 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        diff = 0
-        buy = prices[0]
-        for p in prices:
-            if p < buy:
-                buy = p
-            diff = max(diff, p - buy)
-
-        return diff
+        buy_index = 0
+        max_buy = 0
+        for i in range(len(prices)):
+            new_buy = prices[i] - prices[buy_index]
+            if new_buy < 0:
+                buy_index = i
+            elif new_buy > max_buy:
+                max_buy = new_buy
             
-           
-
-
-
-
-        
+        return max_buy
