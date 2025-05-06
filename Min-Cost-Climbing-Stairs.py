76f1775d30dece783671311384e41fe22def174c
Kaelin Wu <152450138+KaelinWu@users.noreply.github.com>
2025-05-06 18:19:47 -0400

    [746] [Time Beats: 39.12%] [Memory Beats: 86.46%] - LeetPush

---
+++ Min-Cost-Climbing-Stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memo = [0] * (len(cost))
+        if len(cost) == 1:
+            return 0
+        
+        memo[0] = cost[0]
+        memo[1] = cost[1]
+
+        for i in range(2,len(cost)):
+            memo[i] = min(memo[i-1],memo[i-2]) + cost[i]
+        
+        return min(memo[len(cost)-1],memo[len(cost)-2])
