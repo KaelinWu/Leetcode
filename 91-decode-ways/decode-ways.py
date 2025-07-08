9e6bff3bae0c520f0a8fbdf4e2afd28833724265
Kaelin Wu <152450138+KaelinWu@users.noreply.github.com>
2025-07-08 23:31:25 +0200

    Time: 0 ms (100.00%) | Memory: 17.8 MB (69.36%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -2,10 +2,12 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         if s[0] == "0":
             return 0
-        dp = [0] * (len(s)+1)
-        dp[0] = 1
+        n = len(s)
+        dp = [0] * (n+1)
+        dp[0]  = 1
         dp[1] = 1
-        for i in range(2,len(s)+1):
+
+        for i in range(2,n+1):
             one = int(s[i-1])
             two = int(s[i-2:i])
 
@@ -13,5 +15,4 @@ class Solution:
                 dp[i] += dp[i-1]
             if 10 <= two <=26:
                 dp[i] += dp[i-2]
-        return dp[len(s)]
-        
+        return dp[n]
