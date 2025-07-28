74f9ceb4b907db7dd2bd1067cbc78864549aebfd
Kaelin Wu <152450138+KaelinWu@users.noreply.github.com>
2025-07-28 17:49:01 -0400

    Time: 199 ms (63.85%) | Memory: 18 MB (72.31%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount+1)
+        dp[0] = 1
+        for c in coins:
+            for i in range(1, amount+1):
+                if c <= i:
+                    dp[i] += dp[i-c]
+                else:
+                    dp[i] += 0
+        return dp[amount]
