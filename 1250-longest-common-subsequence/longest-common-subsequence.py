b6626d0ffdb3655cf713574a4e496b5d287c6f48
Kaelin Wu <152450138+KaelinWu@users.noreply.github.com>
2025-07-30 16:31:34 -0400

    Time: 355 ms (94.84%) | Memory: 18.1 MB (89.12%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -5,15 +5,16 @@ class Solution:
         if len(text2) > len(text1):
             text1, text2 = text2, text1
             
-        dp = [0] * (len(text2)+1)
+        
         prev = [0] * (len(text2)+1)
 
         for i in range(len(text1)):
+            dp = [0] * (len(text2)+1)
             for j in range(1,len(text2)+1):
                 if text1[i] == text2[j-1]:
                     
                     dp[j] = prev[j-1] + 1
                 else:
                     dp[j] = max(dp[j-1],prev[j])
-            prev,dp = dp,prev
+            prev = dp
         return prev[-1]
