1b0fc1c649b884b26e20e0c6b9e23551acc05a29
Kaelin Wu <152450138+KaelinWu@users.noreply.github.com>
2025-08-06 14:56:08 -0400

    Time: 387 ms (97.19%) | Memory: 18.2 MB (95.62%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,23 +1,23 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        n,node = len(points),0
+        n = len(points)
         distances = [float("infinity")] * n
-        edges, res = 0,0
-        visit = [False] * n
+        visited = [False] * n
+        edges,res = 0,0
+        node = 0
         while edges < n-1:
-            visit[node] = True
             nextnode = -1
+            visited[node] = True
             for i in range(n):
-                if visit[i]:
+                if visited[i]:
                     continue
                 x,y = points[node]
                 x2,y2 = points[i]
                 newdist = abs(x-x2) + abs(y-y2)
-                distances[i] = min(newdist,distances[i])
+                distances[i] = min(distances[i],newdist)
                 if nextnode == -1 or distances[nextnode] > distances[i]:
                     nextnode = i
-            res+=distances[nextnode]
-            node=nextnode
+            res+= distances[nextnode]
             edges+=1
+            node = nextnode
         return res
-
