bb36f2fdebe7e3bd79a56ed9f57868ff098b1203
Kaelin Wu <152450138+KaelinWu@users.noreply.github.com>
2025-08-13 16:59:40 -0400

    Time: 5 ms (72.38%) | Memory: 19.5 MB (27.97%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,20 +1,19 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         adj = defaultdict(list)
+        for source, dest, price in flights:
+            adj[source].append((price,dest))
         prices = [float("infinity")] * n
-        prices[src] = 0
-        for source, destination, price in flights:
-            adj[source].append((price,destination))
-        minheap = deque()
-        minheap.append((0,0,src)) #price, k, src
 
-        while minheap:
-            price,stops,curr = minheap.popleft()
-            if stops > k:
+        que = deque()
+        que.append((src,0,0))
+        while que:
+            curr, visits, price = que.popleft()
+            print(curr)
+            if visits > k:
                 continue
             for new_price, dest in adj[curr]:
-                if new_price+price < prices[dest]:
-                    prices[dest] = new_price +price
-                    minheap.append((price+new_price,stops+1,dest))
-        
+                if price + new_price < prices[dest]:
+                    prices[dest] = price + new_price
+                    que.append((dest,visits+1,prices[dest]))
         return prices[dst] if prices[dst] != float("infinity") else -1
