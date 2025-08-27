609de5f71cffcfedbff024ad55a5fe329bb0cb9c
Kaelin Wu <152450138+KaelinWu@users.noreply.github.com>
2025-08-27 11:47:58 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (69.86%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,18 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        
+        directions = [[0,1], [1,0], [-1,0], [0,-1]]
+        ROW,COL = len(image),len(image[0])
+        
+        org = image[sr][sc]
+        def dfs(row,col):
+            if row < 0 or col < 0 or row >= ROW or col >= COL or image[row][col] != org:
+                return
+            if image[row][col] != org or image[row][col] == color:
+                return
+            image[row][col] = color
+            for dr,dc in directions:
+                dfs(row+dr,col+dc)
+            return
+        dfs(sr,sc)
+        return image
