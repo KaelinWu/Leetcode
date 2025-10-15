a7744859df877d0deb4b8a9534b60716a72808e3
Kaelin Wu <152450138+KaelinWu@users.noreply.github.com>
2025-10-14 21:31:19 -0400

    Time: 0 ms (100.00%) | Memory: 18.9 MB (69.53%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -9,6 +9,6 @@ class Solution:
         if not root:
             return False
         if not root.left and not root.right:
-            return targetSum - root.val == 0
-        targetSum -= root.val
-        return self.hasPathSum(root.left,targetSum) or self.hasPathSum(root.right,targetSum)
+            return root.val == targetSum
+        
+        return self.hasPathSum(root.left,targetSum - root.val) or self.hasPathSum(root.right,targetSum - root.val)
